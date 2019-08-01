5c1c6cab3feadebce6d7486e1f725cea689c024b
shuza <shuza.sa@gmail.com>
2019-08-01 16:48:02 +0600

    response structure changed

--- api/box.go
+++ api/box.go
@@ -13,8 +13,7 @@ func createBox(c *gin.Context) {
 	var box model.Box
 	if err := c.BindJSON(&box); err != nil {
 		log.Warnf("/createBox can't parse request body Error :  %v\n", err)
-		c.JSON(http.StatusOK, gin.H{
-			"status":  http.StatusBadRequest,
+		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "Invalid request body",
 			"data":    err.Error(),
 		})
@@ -24,8 +23,7 @@ func createBox(c *gin.Context) {
 
 	if err := db.Client.Create(box); err != nil {
 		log.Warnf("/createBox can't same in mongoDB Error :  %v\n", err)
-		c.JSON(http.StatusOK, gin.H{
-			"status":  http.StatusInternalServerError,
+		c.JSON(http.StatusInternalServerError, gin.H{
 			"message": "Can't save in DB",
 			"data":    err.Error(),
 		})
@@ -34,7 +32,6 @@ func createBox(c *gin.Context) {
 	}
 
 	c.JSON(http.StatusOK, gin.H{
-		"status":  http.StatusOK,
 		"message": "Box created successfully",
 	})
 }
@@ -43,7 +40,6 @@ func findBox(c *gin.Context) {
 	capacity, err := strconv.ParseInt(c.Query("capacity"), 10, 32)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{
-			"status":  http.StatusBadRequest,
 			"message": "invalid capacity",
 			"data":    err.Error(),
 		})
@@ -65,8 +61,7 @@ func findBox(c *gin.Context) {
 	box, err := db.Client.FindAvailable(int32(capacity), int32(maxWeight))
 	if err != nil {
 		log.Warnf("/findBox can't find box Error :  %v\n", err)
-		c.JSON(http.StatusOK, gin.H{
-			"status":  http.StatusNotFound,
+		c.JSON(http.StatusNotFound, gin.H{
 			"message": "Can't find available box",
 			"data":    err.Error(),
 		})
@@ -74,8 +69,5 @@ func findBox(c *gin.Context) {
 		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"status": http.StatusOK,
-		"data":   box,
-	})
+	c.JSON(http.StatusOK, box)
 }
